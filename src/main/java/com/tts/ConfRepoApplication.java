251a197cf04dda18d640e6a5e6d8a65ea2a757c9
Rommy Yang <rommy.yang@easternphoenix.com>
2018-08-09 15:51:54 +0800

    增加cloud, config

--- src/main/java/com/tts/ConfRepoApplication.java
+++ src/main/java/com/tts/ConfRepoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfRepoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfRepoApplication.class, args);
     }
