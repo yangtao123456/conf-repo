79eb2946da3602b41bf13ae14b1d999fc648b20c
Rommy Yang <rommy.yang@easternphoenix.com>
2018-08-09 15:22:34 +0800

    first commit

---
+++ src/main/java/com/tts/ConfRepoApplication.java
@@ -0,0 +1,12 @@
+package com.tts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfRepoApplication.class, args);
+    }
+}
