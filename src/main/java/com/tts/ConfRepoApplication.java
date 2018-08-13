83c663dfb4dc42fda88d92e8cd26e4463175ed2c
Rommy Yang <rommy.yang@easternphoenix.com>
2018-08-13 11:41:13 +0800

    add rebbitmq

--- src/main/java/com/tts/ConfRepoApplication.java
+++ src/main/java/com/tts/ConfRepoApplication.java
@@ -2,8 +2,12 @@ package com.tts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfRepoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfRepoApplication.class, args);
